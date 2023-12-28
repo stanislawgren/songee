ebeef25f620029d17a2f1041783c42e487a269ce
stanislawgren <stanislawg1200@gmail.com>
2023-12-28 19:55:21 +0100

    nodemon install for dev purposes

--- api/handlers/getHandler.js
+++ api/handlers/getHandler.js
@@ -2,20 +2,6 @@ const fs = require('fs')
 const xpath = require('path')
 
 const getHandler = (response, url) => {
-    
-    let dotoffset = url.lastIndexOf('.')
-    let mimetype =
-        dotoffset == -1
-            ? 'text/html'
-            : {
-                  '.html': 'text/html',
-                  '.ico': 'image/x-icon',
-                  '.jpg': 'image/jpeg',
-                  '.png': 'image/png',
-                  '.gif': 'image/gif',
-                  '.css': 'text/css',
-                  '.js': 'text/javascript',
-              }[url.substr(dotoffset)]
 
     let path = `../../web/public${url}`
 
